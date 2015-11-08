e9f8f6ab1f38fbf1383a2fc833c92c8c471f04fd
Maz Jaleel <mazjaleel@gmail.com>
2015-11-07 21:47:46 -0800

    remove bad UIKit import

--- Datez/Datez/Datez.h
+++ Datez/Datez/Datez.h
@@ -6,7 +6,7 @@
 //  Copyright © 2015 kitz. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Datez.
 FOUNDATION_EXPORT double DatezVersionNumber;
