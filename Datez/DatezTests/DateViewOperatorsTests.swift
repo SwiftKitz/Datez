f6de5c7503807f937777b799914c14d16c72fb45
Maz Jaleel <mazjaleel@gmail.com>
2015-11-08 03:11:54 -0800

    add some DST tests

--- Datez/DatezTests/DateViewOperatorsTests.swift
+++ Datez/DatezTests/DateViewOperatorsTests.swift
@@ -40,4 +40,59 @@ class DateViewOperatorsTests: XCTestCase {
         let expectedDate = anyDate.update(month: 5, day: 19)
         XCTAssertEqual(anyDate + 37.days, expectedDate)
     }
+    
+    func testDateViewOffsetOnDSTDay() {
+        
+        // DST affected time zone
+        let timeZone = NSTimeZone(name: "America/Los_Angeles")!
+        NSTimeZone.setDefaultTimeZone(timeZone)
+        
+        // DST switch date
+        let dstSwitch = DateView(
+            forDateComponents: DateComponents(year: 2015, month: 11, day: 1),
+            inCalendar: NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
+        )
+        
+        // 1 hour will be eated by spending 2 AM twice
+        let expectedDate = DateView(
+            forDateComponents: DateComponents(year: 2015, month: 11, day: 1, hour: 2),
+            inCalendar: NSCalendar.Gregorian
+        )
+        
+        let dstOffset = dstSwitch + 3.hours
+        XCTAssertEqual(dstOffset, expectedDate)
+        
+        NSTimeZone.setDefaultTimeZone(NSTimeZone.systemTimeZone())
+    }
+    
+    func testDateViewOffsetCrossingDSTDay() {
+        
+        // DST affected time zone
+        let timeZone = NSTimeZone(name: "America/Los_Angeles")!
+        NSTimeZone.setDefaultTimeZone(timeZone)
+        
+        let preDst = DateView(
+            forDateComponents: DateComponents(year: 2015, month: 10, day: 29),
+            inCalendar: NSCalendar.Gregorian
+        )
+        
+        let postDst = DateView(
+            forDateComponents: DateComponents(year: 2015, month: 11, day: 3),
+            inCalendar: NSCalendar.Gregorian
+        )
+        
+        // DST compatibile timeZone
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.calendar = NSCalendar.Gregorian
+        dateFormatter.dateStyle = .ShortStyle
+        dateFormatter.timeStyle = .ShortStyle
+        
+        let crossDstForward = preDst + 121.hours
+        XCTAssertEqual(crossDstForward, postDst)
+        
+        let crossDstBackwards = postDst - 121.hours
+        XCTAssertEqual(crossDstBackwards, preDst)
+        
+        NSTimeZone.setDefaultTimeZone(NSTimeZone.systemTimeZone())
+    }
 }
