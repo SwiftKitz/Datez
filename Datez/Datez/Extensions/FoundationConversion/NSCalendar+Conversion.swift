1e8acaefebf886232cc47c6e82bf65b431f28862
Maz Jaleel <mazjaleel@gmail.com>
2015-12-03 05:27:51 -0800

    added all calendars closes #7

--- Datez/Datez/Extensions/FoundationConversion/NSCalendar+Conversion.swift
+++ Datez/Datez/Extensions/FoundationConversion/NSCalendar+Conversion.swift
@@ -23,13 +23,20 @@ extension NSCalendar {
     }
 }
 
-public extension NSCalendar {
+extension NSCalendar {
     
-    static var gregorian: NSCalendar {
-        return NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!
-    }
-    
-    static var islamicCivil: NSCalendar {
-        return NSCalendar(calendarIdentifier: NSCalendarIdentifierIslamicCivil)!
-    }
+    static var gregorian: NSCalendar            { return NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)! }
+    static var buddhist: NSCalendar             { return NSCalendar(calendarIdentifier: NSCalendarIdentifierBuddhist)! }
+    static var chinese: NSCalendar              { return NSCalendar(calendarIdentifier: NSCalendarIdentifierChinese)! }
+    static var coptic: NSCalendar               { return NSCalendar(calendarIdentifier: NSCalendarIdentifierCoptic)! }
+    static var ethiopicAmeteMihret: NSCalendar  { return NSCalendar(calendarIdentifier: NSCalendarIdentifierEthiopicAmeteMihret)! }
+    static var ethiopicAmeteAlem: NSCalendar    { return NSCalendar(calendarIdentifier: NSCalendarIdentifierEthiopicAmeteAlem)! }
+    static var hebrew: NSCalendar               { return NSCalendar(calendarIdentifier: NSCalendarIdentifierHebrew)! }
+    static var ISO8601: NSCalendar              { return NSCalendar(calendarIdentifier: NSCalendarIdentifierISO8601)! }
+    static var indian: NSCalendar               { return NSCalendar(calendarIdentifier: NSCalendarIdentifierIndian)! }
+    static var islamic: NSCalendar              { return NSCalendar(calendarIdentifier: NSCalendarIdentifierIslamic)! }
+    static var islamicCivil: NSCalendar         { return NSCalendar(calendarIdentifier: NSCalendarIdentifierIslamicCivil)! }
+    static var japanese: NSCalendar             { return NSCalendar(calendarIdentifier: NSCalendarIdentifierJapanese)! }
+    static var persian: NSCalendar              { return NSCalendar(calendarIdentifier: NSCalendarIdentifierPersian)! }
+    static var republicOfChina: NSCalendar      { return NSCalendar(calendarIdentifier: NSCalendarIdentifierRepublicOfChina)! }
 }
--- Datez/Datez/Extensions/FoundationConversion/NSDate+Conversion.swift
+++ Datez/Datez/Extensions/FoundationConversion/NSDate+Conversion.swift
@@ -24,11 +24,18 @@ public extension NSDate {
         return dateView(calendar: NSCalendar.currentCalendar())
     }
     
-    public var gregorian: DateView {
-        return dateView(calendar: NSCalendar.gregorian)
-    }
-    
-    public var islamicCivil: DateView {
-        return dateView(calendar: NSCalendar.islamicCivil)
-    }
+    public var gregorian: DateView              { return dateView(calendar: NSCalendar.gregorian) }
+    public var buddhist: DateView               { return dateView(calendar: NSCalendar.buddhist) }
+    public var chinese: DateView                { return dateView(calendar: NSCalendar.chinese) }
+    public var coptic: DateView                 { return dateView(calendar: NSCalendar.coptic) }
+    public var ethiopicAmeteMihret: DateView    { return dateView(calendar: NSCalendar.ethiopicAmeteMihret) }
+    public var ethiopicAmeteAlem: DateView      { return dateView(calendar: NSCalendar.ethiopicAmeteAlem) }
+    public var hebrew: DateView                 { return dateView(calendar: NSCalendar.hebrew) }
+    public var ISO8601: DateView                { return dateView(calendar: NSCalendar.ISO8601) }
+    public var indian: DateView                 { return dateView(calendar: NSCalendar.indian) }
+    public var islamic: DateView                { return dateView(calendar: NSCalendar.islamic) }
+    public var islamicCivil: DateView           { return dateView(calendar: NSCalendar.islamicCivil) }
+    public var japanese: DateView               { return dateView(calendar: NSCalendar.japanese) }
+    public var persian: DateView                { return dateView(calendar: NSCalendar.persian) }
+    public var republicOfChina: DateView        { return dateView(calendar: NSCalendar.republicOfChina) }
 }
